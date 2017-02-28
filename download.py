65402f28d25a68a472c6089df299d7074e5e2ae1
Ps-Pencil <pan.song@dhs.sg>
2017-02-28 23:26:37 +0000

    finished up AirQualityEngland

--- download.py
+++ download.py
@@ -10,11 +10,13 @@ gps['newmarket'] = (52211353,191325)
 gps['montague'] = (52214239,136262)
 gps['rail'] = (52194075,137125)
 gps['parker'] = (52204644,125857)
+
 def std_time(t):
     return map(str, [t.year,(t.date()-date(t.year,1,1)).days,t.hour * 60 + t.minute])
+
+def fix_hour(s):
+    return str(int(s[:2])%24) + s[2:]
 def defra(year):
-    def fix_hour(s):
-        return str(int(s[:2])%24) + s[2:]
     url = "https://uk-air.defra.gov.uk/data_files/site_data/CAM_" + str(year) + ".csv"
     response = urllib2.urlopen(url)
     reader = csv.reader(response)
@@ -61,6 +63,7 @@ def aqe(start,end):
     location_short["Cambridge Newmarket Road"] = "newmarket"
     location_short["Cambridge Montague Road"] = "montague"
 
+    result = []
     # process locations
     for i in range(1,len(locations)):
         if locations[i] in location_short:
@@ -69,80 +72,78 @@ def aqe(start,end):
             locations[i] = locations[i-1]
     print locations,headings
     for row in reader:
-        print row
-
-aqe(datetime(2017,2,27),datetime(2017,2,28))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if len(row) < 2:
+            continue
+        timestamp = row[0] + ' ' + fix_hour(row[1])
+        timestamp = datetime.strptime(timestamp,"%d/%m/%Y %H:%M:%S")
+        heading_to_index = {"NOXasNO2":0,"PM10":1,"PM25":2}
+        # data is keyed by short location string and contains a length-3
+        # list of the data above
+        data = dict()
+        for i,v in enumerate(row[2:]):
+            loc = locations[i+2]
+            hd = headings[i+2]
+            if hd in heading_to_index:
+                index = heading_to_index[hd]
+                if not loc in data:
+                    data[loc] = ["","",""]
+                data[loc][index] = v
+        for loc in data:
+            result.append(std_time(timestamp) + list(gps[loc])+data[loc])
+    return result
+# print aqe(datetime(2017,2,27),datetime(2017,2,28))
 
 
 
-def rail(start,end):
-    """ WARNING: THIS FUNCTION IS NOT USED SINCE THIS SOURCE PROVIDES INSENSIBLE DATA"""
-    """ CHANGING THE COLUMNS SELECTED WILL ACTUALLY CHANGE THE VALUES GIVEN BACK"""
-    from HTMLParser import HTMLParser
-    class TableParser(HTMLParser):
-        result = []
-        in_body = False
-        in_cell = False
-        def handle_starttag(self,tag,attrs):
-            if tag == "tbody":
-                self.in_body = True
-            if self.in_body and tag == "tr":
-                self.result.append([])
-            if self.in_body and tag == "td":
-                self.in_cell = True
-        def handle_endtag(self,tag):
-            if self.in_body and tag == "td":
-                self.in_cell = False
-        def handle_data(self,data):
-            if self.in_cell:
-                self.result[-1].append(data)
-        def get_array(self):
-            return self.result
+# def rail(start,end):
+#     """ WARNING: THIS FUNCTION IS NOT USED SINCE THIS SOURCE PROVIDES INSENSIBLE DATA"""
+#     """ CHANGING THE COLUMNS SELECTED WILL ACTUALLY CHANGE THE VALUES GIVEN BACK"""
+#     from HTMLParser import HTMLParser
+#     class TableParser(HTMLParser):
+#         result = []
+#         in_body = False
+#         in_cell = False
+#         def handle_starttag(self,tag,attrs):
+#             if tag == "tbody":
+#                 self.in_body = True
+#             if self.in_body and tag == "tr":
+#                 self.result.append([])
+#             if self.in_body and tag == "td":
+#                 self.in_cell = True
+#         def handle_endtag(self,tag):
+#             if self.in_body and tag == "td":
+#                 self.in_cell = False
+#         def handle_data(self,data):
+#             if self.in_cell:
+#                 self.result[-1].append(data)
+#         def get_array(self):
+#             return self.result
 
-    def handle_nox(nox):
-        try:
-            nox = float(nox)
-        except ValueError:
-            return ""
-    params = dict()
-    params['station_id'] = 221
-    params['station_name'] = 'Brookgate CB1'
-    params['output_type'] = 'excel'
-    params['report_length'] = 'daily'
-    params['monitors[]'] = [4,119,10,93]
-    params['start_date'] = start.strftime("%d/%m/%Y")
-    params['start_time'] = start.strftime("%H:%M")
-    params['end_date'] = end.strftime("%d/%m/%Y")
-    params['end_time'] = end.strftime("%H:%M")
-    params['result_type'] = 'AVG'
-    params['time_base'] = '1'
-    url = "http://www.wecare4air.co.uk/non-aurn-results"
-    response = requests.post(url,data = params)
-    parser = TableParser()
-    parser.feed(response.text)
-    data = parser.get_array()
-    for row in data:
-        # last element is NOx in ugm-3 unit
-        # value seems wrong
-        # cn't remember which monitor id is this
-        row = row[:-1]
-        timestamp = datetime.strptime(row[0],"%Y-%m-%d %H:%M:%S")
+#     def handle_nox(nox):
+#         try:
+#             nox = float(nox)
+#         except ValueError:
+#             return ""
+#     params = dict()
+#     params['station_id'] = 221
+#     params['station_name'] = 'Brookgate CB1'
+#     params['output_type'] = 'excel'
+#     params['report_length'] = 'daily'
+#     params['monitors[]'] = [4,119,10,93]
+#     params['start_date'] = start.strftime("%d/%m/%Y")
+#     params['start_time'] = start.strftime("%H:%M")
+#     params['end_date'] = end.strftime("%d/%m/%Y")
+#     params['end_time'] = end.strftime("%H:%M")
+#     params['result_type'] = 'AVG'
+#     params['time_base'] = '1'
+#     url = "http://www.wecare4air.co.uk/non-aurn-results"
+#     response = requests.post(url,data = params)
+#     parser = TableParser()
+#     parser.feed(response.text)
+#     data = parser.get_array()
+#     for row in data:
+#         # last element is NOx in ugm-3 unit
+#         # value seems wrong
+#         # cn't remember which monitor id is this
+#         row = row[:-1]
+#         timestamp = datetime.strptime(row[0],"%Y-%m-%d %H:%M:%S")
